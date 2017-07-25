780f52b4cfd5a9b396499971c2f5057ea299f459
jmike <jmike@DESKTOP-54L68HD>
2017-07-25 08:55:23 -0700

    Initial commit. Started Gravity Class with free fall velocity and distance calculators

---
+++ TrajectoryProjector/Gravity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrajectoryProjector
+{
+    class Gravity
+    {
+        public static decimal t;
+        public readonly static decimal g = 9.8M;
+        public static decimal m;
+        public static decimal p = 1.225M;
+        public static decimal A;
+        public static decimal C;
+        
+        public static decimal FreeFallVelocity(decimal timeOfFlightInSeconds)
+        {
+            decimal t = timeOfFlightInSeconds;
+            decimal velocity = g * t;
+
+            return velocity;
+        }
+        
+        public static decimal FreeFallDistance(decimal timeOfFlightInSeconds)
+        {
+            t = timeOfFlightInSeconds;
+            decimal distance = .5M * g * t * t;
+
+            return distance;
+        }
+
+        public static decimal TerminalVelocity(decimal ObjectRadiusInCentimeters, decimal ObjectDensityInGrams)
+        {
+
+        }
+    }
+}
---
+++ TrajectoryProjector/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrajectoryProjector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gravity.Velocity(13) + " m/s It will be {0} meters away", Gravity.Distance(13) );
+        }
+    }
+}
